bdc22fd6f1aceca3a85b4dd67ccfa59a5ac5968d
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2019-11-28 21:09:22 +0900

    Add login / signup

---
+++ src/views/SignUp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <form>
+      <div>
+        <input type="email">
+      </div>
+      <div>
+        <input type="password">
+      </div>
+      <input type="submit">
+    </form>
+
+    <button>Login with Google</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
